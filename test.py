b20937f5824ed05ee7251bc9baa0709dcaeb0556
HerbertNtim <herbertntim15@gmail.com>
2026-02-09 16:04:53 +0000

    routing

--- test.py
+++ test.py
@@ -3,8 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return "Welcome to flask"
+  
+@app.route('/route1')
+def route1():
+  return 'Welcome to route 1'
 
-if __name__:
-  app.run()
+@app.route('/route2')
+def route2():
+  return 'Welcome to route 2'
+
+if __name__ == '__main__':
+  app.run(debug=True)
